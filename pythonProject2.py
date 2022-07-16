01af050f10483c7edf2f9eb70e6ae0e0450dbbf0
Manasa Joshi <68114678+ManasaJoshi-ops@users.noreply.github.com>
2022-07-16 05:48:29 +0530

    Add files via upload
    Tim Urbans Life in weeks:
    Another way to say life is too short

---
+++ pythonProject2.py
@@ -0,0 +1,13 @@
+# Tim Urbans LIFE IN WEEKS project
+
+age=input("What is your present age? ")
+
+remaining_years=90-int(age)
+
+days=365*remaining_years
+weeks=52*remaining_years
+months=12*remaining_years
+
+print(f"You have {days} days, {weeks} weeks, {months} months left")
+
+
